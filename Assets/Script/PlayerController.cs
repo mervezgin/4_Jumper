543f9dfd19eabe16f5d363e3ef8a83ebe528d3ac
mervezgin <mervezgin@gmail.com>
2024-04-17 16:00:01 +0300

    update
    add player, obstacle. move obstacle and background to left

---
+++ Assets/Script/PlayerController.cs
@@ -0,0 +1,35 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerController : MonoBehaviour
+{
+    Rigidbody playerRb;
+
+    [SerializeField] float jumpForce = 10.0f;
+    [SerializeField] float gravityModifier;
+
+    bool isOnGround = true;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        Physics.gravity *= gravityModifier;
+        playerRb = GetComponent<Rigidbody>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (Input.GetKeyDown(KeyCode.Space) && isOnGround)
+        {
+            playerRb.AddForce(Vector3.up * 500, ForceMode.Impulse);
+            isOnGround = false;
+        }
+    }
+
+    void OnCollisionEnter(Collision collision)
+    {
+        isOnGround = true;
+    }
+}
